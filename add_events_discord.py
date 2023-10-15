09d71f53be014473c16b69f5b07fe5be932b8d8b
kareem <kalmasri@me.com>
2023-10-15 16:07:57 -0400

    added slash command to dump events

--- add_events_discord.py
+++ add_events_discord.py
@@ -5,6 +5,7 @@ from tournament_puller import TournamentPuller
 import dotenv
 import os
 import time
+from discord import app_commands
 dotenv.load_dotenv()
 
 errors = []
@@ -20,51 +21,52 @@ if(len(errors) > 0):
 # initiate an object of class TournamentPuller to get tournament data
 tp = TournamentPuller(apikey=os.environ["STARTGGAPIKEY"], state="NC")
 
-# initiate results by state, we already provided state at the initiation
-# step so we dont need to pass that in again here
-# note that we also could have initiated by a list of owner ids
-tp.initiate_by_state("NC")
 
-# after we get state results lets only look at tournaments that run certain games
-# there are also other filter options to choose from that can be used once a
-# list is initiated
-tp.filter_by_game(
-    game_list=['game/street-fighter-6', 'game/guilty-gear-strive'])
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
+    await tree.sync()
     for e in await (await client.fetch_guild(os.environ['GUILD_ID'])).fetch_scheduled_events():
         print(await e.delete())
     print(f'We have logged in as {client.user}')
 
+async def dump_events(guild,guild_events):
+    for t in tp.tournament_list:
+        if([event for event in guild_events if event.name == t['name']]):
+                continue
+            
+        time.sleep(1)
+        await guild.create_scheduled_event(privacy_level=discord.PrivacyLevel.guild_only,
+                                        entity_type=discord.EntityType.external,
+                                        name=t['name'],
+                                        description=f"https://www.start.gg{t['url']}",
+                                        location=f"{t.get('venueAddress','')}",
+                                        start_time=datetime.fromtimestamp(
+                                            t["startAt"]).astimezone(),
+                                        end_time=datetime.fromtimestamp(t["endAt"]).astimezone())
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$dumpevents'):
-        guild = await client.fetch_guild(os.environ['GUILD_ID'])
-        guild_events = await guild.fetch_scheduled_events()
-        for t in tp.tournament_list:
-            if([event for event in guild_events if event.name == t['name']]):
-                    print("already found")
-                    continue
-                
-            print("Adding")
-            time.sleep(1)
-            await guild.create_scheduled_event(privacy_level=discord.PrivacyLevel.guild_only,
-                                               entity_type=discord.EntityType.external,
-                                               name=t['name'],
-                                               description=f"https://www.start.gg{t['url']}",
-                                               location=f"{t.get('venueAddress','')}",
-                                               start_time=datetime.fromtimestamp(
-                                                   t["startAt"]).astimezone(),
-                                               end_time=datetime.fromtimestamp(t["endAt"]).astimezone())
+intents = discord.Intents.default()
+@tree.command(name = "dumpevents", description = "Dumps NC Start.gg Events")
+async def first_command(interaction):
+    
+    await interaction.response.defer(thinking=True)
+    await interaction.followup.send(content="Getting Events from Start.GG")
+    tp.initiate_by_state("NC")
+    tp.filter_by_game(game_list=['game/street-fighter-6', 'game/guilty-gear-strive'])
+    guild = await client.fetch_guild(os.environ['GUILD_ID'])
+    guild_events = await guild.fetch_scheduled_events()
+    await interaction.followup.send(content="Loading Events")
 
+    await dump_events(guild,guild_events)
+    await interaction.followup.send(content="Done")
 client.run(os.environ["BOT_TOKEN"])
